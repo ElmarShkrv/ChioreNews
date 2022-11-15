91435344100257ef813c59e103810a6beeee651e
KotlinCountriesTest <elmarshkrv@gmail.com>
2022-11-15 17:10:22 +0400

    fragment_account_options.xml is done

--- app/src/main/java/com/chiore/chiorenews/fragments/loginRegister/IntroductionFragment.kt
+++
@@ -1,7 +0,0 @@
-package com.chiore.chiorenews.fragments.loginRegister
-
-import androidx.fragment.app.Fragment
-import com.chiore.chiorenews.R
-
-class IntroductionFragment : Fragment(R.layout.fragment_introduction) {
-}
