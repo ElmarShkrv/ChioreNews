1f93cb7a73df66ae4def862eaab9f1c4da1a6764
KotlinCountriesTest <elmarshkrv@gmail.com>
2022-11-22 15:04:38 +0400

    Login and navigation is done

--- app/src/main/java/com/chiore/chiorenews/fragments/loginRegister/AccountOptionsFragment.kt
+++ app/src/main/java/com/chiore/chiorenews/fragments/loginRegister/AccountOptionsFragment.kt
@@ -1,7 +1,37 @@
 package com.chiore.chiorenews.fragments.loginRegister
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.chiore.chiorenews.R
+import com.chiore.chiorenews.databinding.FragmentAccountOptionsBinding
 
 class AccountOptionsFragment : Fragment(R.layout.fragment_account_options) {
+
+    private lateinit var binding: FragmentAccountOptionsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentAccountOptionsBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnLogin.setOnClickListener {
+            findNavController().navigate(R.id.action_accountOptionsFragment_to_loginFragment)
+        }
+
+        binding.btnRegister.setOnClickListener {
+            findNavController().navigate(R.id.action_accountOptionsFragment_to_registerFragment)
+        }
+
+    }
 }
--- app/src/main/java/com/chiore/chiorenews/fragments/loginRegister/LoginFragment.kt
+++ app/src/main/java/com/chiore/chiorenews/fragments/loginRegister/LoginFragment.kt
@@ -1,7 +1,77 @@
 package com.chiore.chiorenews.fragments.loginRegister
 
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.chiore.chiorenews.R
+import com.chiore.chiorenews.activities.NewsActivity
+import com.chiore.chiorenews.databinding.FragmentLoginBinding
+import com.chiore.chiorenews.util.Resource
+import com.chiore.chiorenews.util.longToast
+import com.chiore.chiorenews.util.shortToast
+import com.chiore.chiorenews.viewmodel.LoginViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 
-class LoginFragment : Fragment(R.layout.fragment_login) {
+@AndroidEntryPoint
+class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
+    private val viewmodel by viewModels<LoginViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        binding = FragmentLoginBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.tvDontHaveAccountRegister.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+
+        binding.apply {
+            binding.btnLoginLogin.setOnClickListener {
+                val email = etEmailLogin.text.toString().trim()
+                val password = etPasswordLogin.text.toString()
+                viewmodel.login(email, password)
+            }
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewmodel.login.collect {
+                binding.apply {
+                    when (it) {
+                        is Resource.Loading -> {
+                            loginProgress.visibility = View.VISIBLE
+                        }
+                        is Resource.Success -> {
+                            loginProgress.visibility = View.GONE
+                            Intent(requireActivity(), NewsActivity::class.java).also { intent ->
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                                startActivity(intent)
+                            }
+                        }
+                        is Resource.Error -> {
+                            loginProgress.visibility = View.GONE
+                            requireContext().longToast(it.message.toString())
+                        }
+                        else -> Unit
+                    }
+                }
+            }
+        }
+
+    }
 }
